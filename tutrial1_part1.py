f42501240803da1597e586c789d35cb94cbbd002
Amy webb <c24423456@myTUDublin.ie>
2025-09-24 12:47:38 +0100

    Add files via upload
    tutrial 1 part one introduction into python

---
+++ tutrial1_part1.py
@@ -0,0 +1,35 @@
+i =10
+j = 6
+k= int(i/j)
+
+print(k)
+f = 10.5
+
+i = i + 10
+i += 7
+i -= 3
+i += j
+i *= 2.5
+j /= 3
+i += j
+print(i)
+print(j)
+
+b:bool = True
+
+b = (i > 9)
+
+s:str = "Amy"
+print(s)
+
+sf = f"Hello (s)"
+
+if i < 0:
+    print("i is negative")
+
+if i >= 0:
+    print("i is positive")
+
+if i >= 0 and i < 10:
+    print("i is berween 0 and 10")
+
