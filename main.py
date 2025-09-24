088dde2e0c667a5b9f631469add02f837b3cca49
Amy webb <c24423456@myTUDublin.ie>
2025-09-24 12:35:19 +0100

    Add files via upload
    lecture week1 python code

---
+++ main.py
@@ -0,0 +1,30 @@
+import py5
+
+def setup():
+   import py5
+
+def setup():
+    py5.size(400, 400)               
+    py5.background(255, 0, 0)        #background red
+    py5.fill(255,255,0)              # circle colour yellow
+    py5.circle(200, 200, 300)        # cicrle
+    py5.fill(0, 255, 225)            # triangle colour blue
+    py5.triangle(50, 350, 200, 50, 350, 350) #triangle formation
+    py5.fill(180)  # grey color for ellipse
+    py5.ellipse(200, 200, 180, 100)
+    py5.fill(0) #black for pupil
+    py5.circle(200, 200, 60) #second smaller circle
+
+py5.run_sketch()
+
+   
+    
+#def draw():
+   # py5.size(400, 400)
+   # py5.background(255)
+    #py5.fill(255,255,0)
+    #py5.circle(200,200,100)
+    # py5.rect(150,150,60,30)
+   # py5.no_fill()
+   # py5.triangle(10,30,350,250,10,350)
+   
